cdc5f6d3f3516b4de37049eeef23e1af37013855
S-Yajima <shige048jp@gmail.com>
2020-01-29 19:59:08 +0900

    iOSで操作するUI

--- move_round_angle_iOS.py
+++ move_round_angle_iOS.py
@@ -1,19 +1,20 @@
-# 描画した円を移動させる
-# 描画した円をメインビューの端で反射させ進行方向を更新する
-# 反射後の進行方向は円がもつ現在の進行方向から計算する
-# 進行方向の角度を直角三角形の斜辺とみたて、x軸とy軸の移動する数値を計算する
 
 import ui
 import threading
 import math
 
+
+# 円を描画するView
+# 進行方向の角度と速度、色を保持する
+# 円の描画はdraw()メソッド内で.ui.Pathクラスにて行う
 class MyView(ui.View):
     draw_color = 'yellow'
-    angle = 45
+    angle = 0
     speed = 1
     movement_x = 1.0
-    movement_y = 1.0
+    movement_y = 0.0
 
+    # 移動速度と角度,色を設定する
     def set_speed_angle_color(self, speed, angle, draw_color):
         self.speed = speed
         self.angle = angle
@@ -27,8 +28,8 @@ class MyView(ui.View):
         return (self.y + self.height)
 
     # 角度と速度から、x軸y軸に動く長さを計算する
-    # x軸= cos 角度
-    # y軸= sin 角度
+    # x軸 = cos 角度
+    # y軸 = sin 角度
     def calc_movement(self):
         if self.angle == 0:
             self.movement_x = 1.0
@@ -46,16 +47,23 @@ class MyView(ui.View):
             self.movement_x = math.cos(math.radians(self.angle))
             self.movement_y = math.sin(math.radians(self.angle))
 
+    # 接触した壁の角度から、反射角度を計算して設定する
+    def angle_with_wallangle(self, wallangle):
+        new_angle = (self.angle + ((wallangle - self.angle) * 2)) % 360
+        # print(new_angle)
+        return new_angle
+
     # メインビューの端に到達した際の角度の変更
     def reflect_main(self):
+        # メインビューの左右の壁に接触した場合は
+        # 90°の壁に接触した前提で反射角度を計算し設定する
         if self.x <= 0 or self.upper_right() >= self.superview.width:
-            if self.angle <= 180:
-                self.angle = 180 - self.angle
-            else:
-                self.angle = 180 + (360 - self.angle)
+            self.angle = self.angle_with_wallangle(90)
 
+        # メインビューの上下の壁に接触した場合は
+        # 0°の壁に接触した前提で反射角度を計算し設定する
         if self.y <= 0 or self.lower_left() >= self.superview.height:
-            self.angle = 360 - self.angle
+            self.angle = self.angle_with_wallangle(0)
 
         self.calc_movement()
 
@@ -70,8 +78,7 @@ class MyView(ui.View):
         self.x = self.x + (self.movement_x * self.speed)
         self.y = self.y + (self.movement_y * self.speed)
 
-# Timerに呼び出される処理
-# 円の描画と移動を行う
+
 def schedule(main_view):
     for sub_view in main_view.subviews:
         sub_view.reflect_main()
@@ -89,8 +96,9 @@ def schedule(main_view):
 if __name__ == '__main__':
     # メイン画面の作成
     main_view = ui.View(frame=(0, 0, 375, 667))
-    main_view.name = 'Viewに進む角度を設定した円を描画し動かす'
+    main_view.name = '進む角度を設定した円を描画し動かす'
     main_view.background_color = 'lightblue'
+    # main_view.background_color = 'black'
 
     sub_view_1 = MyView(frame=(100, 100, 70, 70))
     sub_view_1.set_speed_angle_color(3, 20, 'yellow')
